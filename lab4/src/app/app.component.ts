a6718f34c04d0a56096911e870dcad4d08819743
Yerasyil Omirzhanov <65915633+Pingwinnel@users.noreply.github.com>
2024-02-20 23:28:30 +0600

    lab4_update

---
+++ lab4/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  images = [
+    {
+      imageSrc:
+        'https://resources.cdn-kaspi.kz/img/m/p/h8e/h73/85124918935582.jpg?format=gallery-medium',
+    },
+    {
+      imageSrc:
+        'https://resources.cdn-kaspi.kz/img/m/p/h8e/h73/85124918935582.jpg?format=gallery-medium',
+    },
+    {
+      imageSrc:
+        'https://resources.cdn-kaspi.kz/img/m/p/h8e/h73/85124918935582.jpg?format=gallery-medium',
+    },
+    {
+      imageSrc:
+        'https://resources.cdn-kaspi.kz/img/m/p/h8e/h73/85124918935582.jpg?format=gallery-medium',
+    },
+    {
+      imageSrc:
+        'https://resources.cdn-kaspi.kz/img/m/p/h8e/h73/85124918935582.jpg?format=gallery-medium',
+    },
+  ];
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
---
+++ lab4/src/app/product-list/product-list.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+import { Product, products } from '../products';
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = [...products];
+  share() {
+    window.alert('The product has been shared!');
+  }
+  shareOnWhatsApp(product: Product) {
+    const productName = encodeURIComponent(product.name);
+    const productLink = encodeURIComponent(product.url_product);
+
+    const whatsappUrl = `https://wa.me/?text=${productName}%20-%20${productLink}`;
+    window.open(whatsappUrl, '_blank');
+  }
+
+  // Method to share on Telegram
+  shareOnTelegram(product: Product) {
+    const productName = encodeURIComponent(product.name);
+    const productLink = encodeURIComponent(product.url_product);
+
+    const telegramUrl = `https://t.me/share/url?url=${productLink}&text=${productName}`;
+    window.open(telegramUrl, '_blank');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
