1850676667708bc8ad00031c08cb573efa89512d
Yerasyil Omirzhanov <65915633+Pingwinnel@users.noreply.github.com>
2024-04-02 22:25:33 +0500

    lab8_updated

--- lab8/api/models.py
+++ lab8/api/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
@@ -11,5 +13,7 @@ class Product(models.Model):
     count = models.IntegerField(default=0)
     is_active = models.BooleanField(default=True)
     category_id = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    def __str__(self):
+        return self.name
 
 
--- lab8/api/urls.py
+++ lab8/api/urls.py
@@ -9,7 +9,7 @@ from .views import (
 
 urlpatterns = [
     path('products/', ProductViewCreate.as_view(), name='Product_List'),
-    path('product/<int:pk>/', ProductRetrieveUpdateDestroy.as_view(), name='Product_Detail'),
+    path('products/<int:pk>/', ProductRetrieveUpdateDestroy.as_view(), name='Product_Detail'),
     path('categories/', CategoryViewSet.as_view(), name='Categories_list'),
     path('categories/<int:pk>/', CategoryRetriveUpdateDelete.as_view(), name='Category_Detail'),
     path('categories/<int:id>/products/', CategoryProductListView.as_view(), name='Category_Products_List'),
